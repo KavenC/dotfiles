eab59c2d88ad8d8771214596889555fae23d4275
Chun-Kai Chou <chou@silexica.com>
2017-04-12 14:16:24 +0200

    add emacs.d folder

---
+++ .emacs.d/elpa/flycheck-rtags-20170403.957/flycheck-rtags-autoloads.el
@@ -0,0 +1,16 @@
+;;; flycheck-rtags-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("flycheck-rtags.el") (22765 62900 646823
+;;;;;;  842000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-rtags-autoloads.el ends here
---
+++ .emacs.d/elpa/helm-rtags-20170402.653/helm-rtags-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-rtags" "20170402.653" "A front-end for rtags" '((helm "2.0") (rtags "2.9")) :url "http://rtags.net")
